1724bb99edf7d002bfd370048a8e840477654256
Whidy <whidy@whidy.net>
2019-05-29 08:10:14 +0800

    sync

---
+++ hide-me/index.js
@@ -0,0 +1,47 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+
+// To modify the proxy connection before data is sent, you can listen
+// for the 'proxyReq' event. When the event is fired, you will receive
+// the following arguments:
+// (http.ClientRequest proxyReq, http.IncomingMessage req,
+//  http.ServerResponse res, Object options). This mechanism is useful when
+// you need to modify the proxy request before the proxy connection
+// is made to the target.
+//
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  console.log(req.headers);
+  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+});
+
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  proxy.web(req, res, {
+    target: 'http://127.0.0.1:7001'
+  });
+});
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // console.log(req.headers);
+  res.write('request successfully proxied! this is 5060 content' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(5060);
+console.log("listening on port 5050")
+server.listen(5050);
+
+
+var server2 = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  proxy.web(req, res, {
+    target: 'http://127.0.0.1:5050'
+  });
+});
+
+server2.listen(5049);

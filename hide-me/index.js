17946c7fa3cc238437d8c026650b249e179b4e07
Whidy_Macbook <whidy@whidy.net>
2019-06-02 22:53:08 +0800

    http-proxy test

--- hide-me/index.js
+++ hide-me/index.js
@@ -2,46 +2,26 @@ var http = require('http'),
     httpProxy = require('http-proxy');
 
 //
-// Create a proxy server with custom application logic
+// Create a proxy server with latency
 //
-var proxy = httpProxy.createProxyServer({});
+var proxy = httpProxy.createProxyServer();
 
-// To modify the proxy connection before data is sent, you can listen
-// for the 'proxyReq' event. When the event is fired, you will receive
-// the following arguments:
-// (http.ClientRequest proxyReq, http.IncomingMessage req,
-//  http.ServerResponse res, Object options). This mechanism is useful when
-// you need to modify the proxy request before the proxy connection
-// is made to the target.
 //
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  console.log(req.headers);
-  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
-});
-
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
+// Create your server that makes an operation that waits a while
+// and then proxies the request
+//
+http.createServer(function (req, res) {
+  console.log(req.url);
   proxy.web(req, res, {
-    target: 'http://127.0.0.1:7001'
+    target: req.url
   });
-});
+}).listen(8008);
+
+//
+// Create your target server
+//
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  // console.log(req.headers);
-  res.write('request successfully proxied! this is 5060 content' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
   res.end();
-}).listen(5060);
-console.log("listening on port 5050")
-server.listen(5050);
-
-
-var server2 = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-  proxy.web(req, res, {
-    target: 'http://127.0.0.1:5050'
-  });
-});
-
-server2.listen(5049);
+}).listen(9008);
